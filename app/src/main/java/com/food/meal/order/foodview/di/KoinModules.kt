4eede9fb27f1c73af36ff8a3e7ea0f310142a11a
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-10-15 10:33:33 +0300

    Добавлен класс NetworkStatus.kt для определеняи состояния сети Интернет.

--- app/src/main/java/com/food/meal/order/foodview/di/KoinModules.kt
+++ app/src/main/java/com/food/meal/order/foodview/di/KoinModules.kt
@@ -1,6 +1,12 @@
 package com.food.meal.order.foodview.di
 
+import com.food.meal.order.foodview.utils.network.NetworkStatus
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val application = module {
+
+    // Вспомогательные классы:
+    // Определение статуса сети
+    single<NetworkStatus> { NetworkStatus(androidContext()) }
 }
