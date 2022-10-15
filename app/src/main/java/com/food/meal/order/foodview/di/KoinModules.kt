47dba0964d03c36739abcc77371c55fef961bf0c
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2022-10-15 10:37:23 +0300

    Добавлен класс ResourcesProvider.kt для доступа к ресурсам и контексту в приложении.

--- app/src/main/java/com/food/meal/order/foodview/di/KoinModules.kt
+++ app/src/main/java/com/food/meal/order/foodview/di/KoinModules.kt
@@ -1,6 +1,8 @@
 package com.food.meal.order.foodview.di
 
 import com.food.meal.order.foodview.utils.network.NetworkStatus
+import com.food.meal.order.foodview.utils.resources.ResourcesProvider
+import com.food.meal.order.foodview.utils.resources.ResourcesProviderImpl
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
@@ -9,4 +11,6 @@ val application = module {
     // Вспомогательные классы:
     // Определение статуса сети
     single<NetworkStatus> { NetworkStatus(androidContext()) }
+    // Получение доступа к ресурсам
+    single<ResourcesProvider> { ResourcesProviderImpl(androidContext()) }
 }
